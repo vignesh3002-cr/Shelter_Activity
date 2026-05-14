54ed29172a5275df00a798d3a67ff440bbb3b070
Vignesh Vicky <vigneshvickey2004@gmail.com>
2026-05-14 18:02:59 +0530

    loading effect

--- src/pages/ProjectMasterPage.jsx
+++ src/pages/ProjectMasterPage.jsx
@@ -117,7 +117,8 @@ function ProgressBar({status}) {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      {loading && (<motion.img
+      {loading && (<div className='flex items-center justify-center h-screen bg-white'><motion.img
+        className="w-32 h-32 mx-auto"
         src="/Shelter_logo.png"
            animate={{
              scale: [1, 1.2, 1],
@@ -127,7 +128,7 @@ function ProgressBar({status}) {
            duration: 1.5,
           repeat: Infinity,
        }}
-   ></motion.img>)}
+   ></motion.img></div>)}
       {/* Top Nav */}
       <header className="sticky top-0 z-20 bg-white border-b border-gray-200">
         <div className="flex items-center justify-between h-16 max-w-6xl px-2 mx-auto md:px-6">
